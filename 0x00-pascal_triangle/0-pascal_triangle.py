0d3fda76de1149ff39114f36556f9281f1faed20
Patrick Odeh <vyltyan@gmail.com>
2024-10-03 18:15:24 +0100

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,8 +8,9 @@ def pascal_triangle(n):
     triangle = [[1]]
     for i in range(1, n):
         row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        for j in range(len(triangle[i - 1]) - 1):
+
+            row.append(triangle[i - 1][j] + triangle[i - 1][j + 1])
         row.append(1)
         triangle.append(row)
 
